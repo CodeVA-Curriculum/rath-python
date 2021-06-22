a492eef32db745d21a241963a40df821a3f00638
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-06-22 14:04:39 -0400

    added bulma & began basic layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,38 @@
-
+<script>
+	import "../global.scss";
+</script>
 
 
 
 <main>
-	<slot />
+	<nav class="navbar" role="navigation" aria-label="main navigation">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="https://bulma.io">
+				<img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+			</a>
+	  
+			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</a>
+		</div>
+	  
+		<div id="navbarBasicExample" class="navbar-menu">
+			<div class="navbar-start">
+				
+		 	</div>
+			<div class="navbar-end">
+				<a class="navbar-item">
+					Home
+				</a>
+				<a class="navbar-item">
+					Documentation
+				</a>
+			</div>
+		</div>
+	</nav>
+	<div class='section'>
+		<slot />
+	</div>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,4 @@
-<h1 class="title">Hello, Svelte</h1>
+<div class='container is-max-desktop'>
+	<h1 class="title">Hello, Svelte </h1>
+	<button class='button'>Click Me</button>
+</div>
