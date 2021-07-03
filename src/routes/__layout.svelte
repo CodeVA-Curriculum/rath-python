711d28dba680a303ce842157af182cf3441af146
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-07-03 12:27:41 -0400

    Fixed links

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,9 @@
 	import pyLogo from '$lib/assets/Python Logo.png';
 	import {faPlus} from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
+
+	import {base} from "$app/paths";
+	$: console.log(base);
 </script>
 
 
@@ -12,12 +15,12 @@
 <main class='is-flex is-flex-direction-column'>
 	<nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
 		<div class="navbar-brand">
-			<a class="navbar-item" href='.'>
-				<img src={logoSrc} height="28">
+			<a class="navbar-item" href="/">
+				<img src={logoSrc} alt="A logo for Code Virginia--a large blue circle with the text 'CodeVA' in the center" height="28">
 				<div class='px-2'>
 					<Fa icon={faPlus} size="1.5x" />
 				</div>
-				<img src={pyLogo} height="28">
+				<img src={pyLogo} alt="The Python logo--two stylized snakes forming a 'plus' sign" height="28">
 			</a>
 	  
 			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -31,13 +34,27 @@
 			<div class="navbar-start">
 				
 		 	</div>
-			<div class="navbar-end">
-				<a href='.' class="navbar-item">
+			<div class="navbar-end links">
+				<a href='/' class="navbar-item">
 					Home
 				</a>
-				<!-- <a href='/about' class="navbar-item">
-					Overview
-				</a> -->
+
+				<!-- Dropdown -->
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a href="{base}/modules" class="navbar-link">
+					Modules
+					</a>
+			
+					<div class="navbar-dropdown">
+					<a href="{base}/modules/module-0" class="navbar-item">
+						0. Course Overview
+					</a>
+					<a href="{base}/modules/module-1" class="navbar-item">
+						1. Speaking & Listening
+					</a>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</nav>
@@ -60,5 +77,8 @@
 	}
 	main > footer {
 		margin-top: auto;
+	}
+	.links {
+		margin-right: 5rem;
 	}
 </style>
