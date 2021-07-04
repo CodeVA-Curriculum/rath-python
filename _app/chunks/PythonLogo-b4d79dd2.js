0b1d4996f99e36bc0ad54b48f0af0d4a9b6a8a83
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-07-04 17:02:30 -0400

    Updates

--- _app/chunks/PythonLogo-b4d79dd2.js
+++ _app/chunks/PythonLogo-b4d79dd2.js
@@ -1,1 +1,1 @@
-var p="/_app/assets/CodeVALogo.cdf06ed4.png",s="/_app/assets/PythonLogo.980158f6.png";export{p as c,s as p};
+var p="/rath-python/_app/assets/CodeVALogo.cdf06ed4.png",a="/rath-python/_app/assets/PythonLogo.980158f6.png";export{p as c,a as p};
