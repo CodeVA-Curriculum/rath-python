cd9dfb2f01b6cc778b848ff3c92cc9b038dcb71b
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-07-23 09:42:28 -0400

    Finished content

---
+++ src/lib/components/ReplitEmbed.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let url;
+</script>
+
+{#if url}
+<div class='replit-embed container my-5 px-0'>
+    <iframe frameborder="0" width="100%" height="500px" src="{url}?embed=true&outputonly=1"></iframe>
+</div>
+{/if}
